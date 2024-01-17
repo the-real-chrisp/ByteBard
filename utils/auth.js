cad8d769d9c583555648bf816fcbeb8fc873679c
Chris Purcell <chrisp0000@gmail.com>
2024-01-16 16:55:48 -0800

    Created authorization util

---
+++ utils/auth.js
@@ -0,0 +1,10 @@
+const auth = (req, res, next) => {
+    if (!req.session.logged_in) {
+      res.redirect('/');
+      return
+    } else {
+      next();
+    }
+  };
+  
+  module.exports = auth;
