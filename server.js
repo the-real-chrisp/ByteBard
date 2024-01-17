1188cf0360c3425b0362e563866e80fb4e45a7d2
Chris Purcell <chrisp0000@gmail.com>
2024-01-16 16:45:31 -0800

    Created server

---
+++ server.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const hbs = exphbs.create({ helpers });
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {
+    maxAge: 1 * 60 * 60 * 1000,
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+function list(path) {
+  const Model = require(path)
+  Model.findAll({}).then(items => {
+    console.log(`\n${path}\n`)
+    console.log(items.map(item => item.dataValues))
+  })
+}
+
+sequelize.sync({ alter: true }).then(() => {
+  app.listen(PORT, () => function(err){
+    if (err) {
+      console.log(err)
+    }
+    console.log(`Now listening on port ${PORT} http://localhost:${PORT}/`)
+    }
+  );
+});
