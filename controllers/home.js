ae24bb3f200e9d4d42c6ef915855efbe1bacde8a
Chris Purcell <chrisp0000@gmail.com>
2024-02-07 17:31:17 -0800

    Getting app to display

--- controllers/home.js
+++ controllers/home.js
@@ -1,36 +1,37 @@
 const router = require('express').Router();
 const { Post } = require('../models');
-const withAuth = require('../utils/auth');
+// const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
   try {
-    const postData = await Post.findAll({
-      include: [
-        {
-            model: 'post',
-            attribute: ['title', 'message', 'date_created', 'user_id']
-        }
-      ]
-    });
+    res.render('layouts/main')
+    // const postData = await Post.findAll({
+    //   include: [
+    //     {
+    //         model: 'post',
+    //         attribute: ['title', 'message', 'date_created', 'user_id']
+    //     }
+    //   ]
+    // });
 
-    const posts = postData.map((post) => post.get({ plain: true }));
+    // const posts = postData.map((post) => post.get({ plain: true }));
 
-    res.render('homepage', {
-      posts,
-      logged_in: req.session.logged_in,
-    });
+    // res.render('main', {
+    //   posts,
+    //   logged_in: req.session.logged_in,
+    // });
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.get('/login', (req, res) => {
-  if (req.session.logged_in) {
-    res.redirect('/');
-    return;
-  }
+// router.get('/login', (req, res) => {
+//   if (req.session.logged_in) {
+//     res.redirect('/');
+//     return;
+//   }
 
-  res.render('login');
-});
+//   res.render('login');
+// });
 
 module.exports = router;
